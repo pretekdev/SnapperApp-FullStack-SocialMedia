f3775553683fbd8074259cabdb2763d51d3764bd
pretekdev <kumar@uleth.ca>
2024-01-27 12:12:41 -0700

    updates

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import Topbar from '@/components/shared/Topbar';
+import { Outlet } from "react-router-dom";
+import Bottombar from "@/components/shared/Bottombar";
 
 const RootLayout = () => {
   return (
-    <div>
-      RootLayout
-    </div>
-  )
-}
+    <div className="w-full md:flex">
+      <Topbar />
+      <LeftSidebar />
 
-export default RootLayout
+      <section className="flex flex-1 h-full">
+        <Outlet />
+      </section>
+
+      <Bottombar />
+    </div>
+  );
+};
+
+export default RootLayout;
--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,10 +1,11 @@
+
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      
-    </div>
+   <div>
+    Home
+   </div>
   )
 }
 
